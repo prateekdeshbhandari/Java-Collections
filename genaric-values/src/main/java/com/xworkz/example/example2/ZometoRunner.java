e399bc037d5167f15d3157c4b5961956385e1742
prateekdeshbhandari <prateekdeshbhandari@gmail.com>
2026-06-26 19:25:37 +0530

    Implemented generic values examples

--- genaric-values/src/main/java/com/xworkz/example/example2/ZometoRunner.java
+++ genaric-values/src/main/java/com/xworkz/example/example2/ZometoRunner.java
@@ -1,8 +1,11 @@
 package com.xworkz.example.example2;
 
+import java.util.Collection;
+
 public class ZometoRunner {
 
     public static void main(String[] args) {
         Zometo<String,Long> zometo=new Zometo<>("pranav",234567789l,"j p nagar");
+
     }
 }
