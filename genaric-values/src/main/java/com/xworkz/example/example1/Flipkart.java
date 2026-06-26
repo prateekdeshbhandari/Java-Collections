25203041e20db72b498373e5b0e323e112645538
prateekdeshbhandari <prateekdeshbhandari@gmail.com>
2026-06-26 13:43:41 +0530

    Implemented generic values examples

---
+++ genaric-values/src/main/java/com/xworkz/example/example1/Flipkart.java
@@ -0,0 +1,14 @@
+package com.xworkz.example.example1;
+
+public class Flipkart {
+
+    int orderId;
+    String orderName;
+    long contactNumber;
+
+    void onlineOrderApp() {
+        System.out.println(orderId);
+        System.out.println(orderName);
+        System.out.println(contactNumber);
+    }
+}
---
+++ genaric-values/src/main/java/com/xworkz/example/example1/FlipkartRunner.java
@@ -0,0 +1,15 @@
+package com.xworkz.example.example1;
+
+public class FlipkartRunner {
+
+
+    public static void main(String[] args) {
+        // Flipkart.onlineOrderApp(2,"prateek",86778752185L);
+        Flipkart order=new Flipkart();
+        order.orderId=1;
+
+        order.onlineOrderApp();
+    }
+}
+
+
